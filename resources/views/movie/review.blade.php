9bf1d8661ea85bbe80f775258e39491579a44f77
h.odahara <odahara178@gmail.com>
2019-04-20 21:23:17 +0900

    レビューレイアウトの作成完了

--- resources/views/movie/review.blade.php
+++ resources/views/movie/review.blade.php
@@ -4,36 +4,40 @@
 <h1>レビュー投稿ページ</h1>
 
 
-    <div class="container">
+    <div class="container border-bottom pb-2">
+
+    <hr>
+
+        <div class="review">
+            <table>
+                <tr>
+                    <th>ユーザー名</th>
+                    <th style="color: #009900;">〇〇</th>
+                </tr>
+                <tr>
+                    <td colspan="3">レビュー内容</td>                      
+                </tr>
+            </table>
+        </div>
+
+    </div>
+          
+    <div class="container mt-4">
 
     
-        <div class="bbs">
-                <table>
-                    <tr>
-                        <th>/th>
-                        <th style="color: #009900;"></th>
-                        <th>：</th>
-                    </tr>
-                    <tr>
-                        <td colspan="3"></td>                      
-                    </tr>
-                    <tr>
-                        <td colspan="3"></td>
-                    </tr>
-                </table>
-        </div>
-            
-    {{-- 投稿フォーム --}}
+        {{-- 投稿フォーム --}}
         <div class="posted_form">
             <form class="form" method="post">
                 @csrf
-                <input type="submit" value="投稿する">
+                <input class="btn btn-info" type="submit" value="レビューする">
                 名前：<input name="name">
                 <br>
-                <textarea name="content" cols="60" rows="10"></textarea>
+                <textarea class="mt-1" name="content" cols="60" rows="10"></textarea>
             </form>
         </div>
+
     </div>
+    
 
 
 
