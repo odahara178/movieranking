f802cf8320bf6e903d7619df14461ffac807c939
h.odahara <odahara178@gmail.com>
2019-05-27 21:40:34 +0900

    ランキングバッジ処理追加

--- app/Ranking.php
+++ app/Ranking.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ranking extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'genre', 'rank', 'movie_id', 'evaluation'
+    ];
 }
