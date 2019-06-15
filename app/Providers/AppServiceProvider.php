289294f65f0a3be4c8c52b2a6b5ec019341b7b75
h.odahara <odahara178@gmail.com>
2019-06-15 19:16:01 +0900

    AppServiceProvider.php変更

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,5 +27,7 @@ class AppServiceProvider extends ServiceProvider
         if (\App::environment('production')) {
             \URL::forceScheme('https');
         }
+        // Herokuのmigrationを通す為
+        Schema::defaultStringLength(191);
     }
 }
